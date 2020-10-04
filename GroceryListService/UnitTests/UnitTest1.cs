b3fbfaba086ed9ad871137f883996709e621bbe6
JonahBurke <jonahburke777@gmail.com>
2020-10-04 16:42:02 -0500

    Created unit testing file and added a few calibration unit tests to be replaced later

---
+++ GroceryListService/UnitTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GroceryListService.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void passingTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void failingTest()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
