523528936e4c2c9f8f2ec2f76a85b7a6cf3b0aee
Jacob(Jiakai) <jacoblee5286@gmail.com>
2020-10-07 02:39:27 -0500

    Engine added

---
+++ GroceryListService/Engine/ListEngine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GroceryListService.Models;
+
+namespace GroceryListService.Engine
+{
+    public class ListEngine
+    {
+        public void createlist(int id, string name, int Userid, Dictionary<int, string> Item)
+        {
+            List newlist = new List();
+            newlist.Id = id;
+            newlist.Name = name;
+            newlist.UserId = Userid;
+            newlist.item = Item;
+
+        }
+    }
+}
--- GroceryListService/Models/List.cs
+++ GroceryListService/Models/List.cs
@@ -9,5 +9,7 @@ namespace GroceryListService.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int UserId { get; set; }
+        public Dictionary<int, string> item { get; set; }
     }
 }
