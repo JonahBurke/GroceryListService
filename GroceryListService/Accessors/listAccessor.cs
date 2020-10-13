02d41e5823979e4340a2e27d76326927751c203d
JonahBurke <jonahburke777@gmail.com>
2020-10-13 14:20:16 -0500

    Refactored the accessor classes to better adhere to dependency injection
    Also removed the UnitTests1 class because it wasn't necessary.

--- GroceryListService/Accessors/listAccessor.cs
+++ GroceryListService/Accessors/listAccessor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,7 +8,7 @@ namespace GroceryListService.Accessors
 {
     public class listAccessor : dbAccessor
     {
-        public listAccessor(string connectionString) : base(connectionString)
+        public listAccessor(SqlConnection connection) : base(connection)
         {
             //TODO
         }
