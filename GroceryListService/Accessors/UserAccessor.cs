e22fbb73db20dcda516ac1fe829fe4f0c78064b0
JonahBurke <jonahburke777@gmail.com>
2020-10-28 14:54:18 -0500

    Removed unecessary usings in several files

--- GroceryListService/Accessors/UserAccessor.cs
+++ GroceryListService/Accessors/UserAccessor.cs
@@ -1,10 +1,5 @@
-﻿using _GroceryListService.Models;
-using GroceryListService.Models;
-using Microsoft.Data.SqlClient;
+﻿using Microsoft.Data.SqlClient;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GroceryListService.Accessors
 {
--- GroceryListService/Accessors/itemAccessor.cs
+++ GroceryListService/Accessors/itemAccessor.cs
@@ -2,8 +2,6 @@
 using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GroceryListService.Accessors
 {
