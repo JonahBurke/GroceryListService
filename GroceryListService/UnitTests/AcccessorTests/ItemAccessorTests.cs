a3590579db668b18a7821c1b4738507f8b44a1cf
JonahBurke <jonahburke777@gmail.com>
2020-11-05 16:11:09 -0600

    Added up-to-date tests back in

--- GroceryListService/UnitTests/AcccessorTests/ItemAccessorTests.cs
+++ GroceryListService/UnitTests/AcccessorTests/ItemAccessorTests.cs
@@ -3,10 +3,8 @@ using GroceryListService.Data;
 using GroceryListService.Models;
 using Microsoft.Data.SqlClient;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace GroceryListService.UnitTests
 {
@@ -16,26 +14,43 @@ namespace GroceryListService.UnitTests
         [TestMethod]
         public void test_item_exists()
         {
+            // setup
             ItemAccessor ia = new ItemAccessor(new SqlConnection(DatabaseInfo.connectionString));
-            string itemName = "Item 1";
-            List testList = new List
-            {
-                Name = "List 1",
-                UserId = 1
-            };
-            Assert.IsTrue(ia.ItemExists(itemName, testList));
+            int id = 1;
+            // Act and assert are heppening simultaneously here
+            Assert.IsTrue(ia.ItemExists(id));
         }
+
         [TestMethod]
-        public void test_item_remove()
+        public void test_select_item()
         {
+            // setup
             ItemAccessor ia = new ItemAccessor(new SqlConnection(DatabaseInfo.connectionString));
+            int id = 1;
             string itemName = "Item 1";
+            //Act
+            Item i = ia.SelectItem(id);
+            // Assert for each of the qualities of Item
+            Assert.AreEqual(itemName, i.Name);
+            Assert.AreEqual(1, i.Id);
+            Assert.AreEqual(-1, i.Quantity);
+        }
+
+        [TestMethod]
+        public void test_select_all_items()
+        {
+            // setup
+            ItemAccessor ia = new ItemAccessor(new SqlConnection(DatabaseInfo.connectionString));
             List testList = new List
             {
                 Name = "List 1",
                 UserId = 1
             };
-            Assert.IsTrue(ia.RemoveItem(itemName, testList));
+            //Act
+            List<Item> itemList = ia.SelectAllItems(testList);
+            // Assert that we should get 2 items from the database here
+            Assert.AreEqual(2, itemList.Count());
+
         }
     }
 }
