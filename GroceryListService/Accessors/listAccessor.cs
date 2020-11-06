754593a73041137a83403bcb45f7a1e8a5692be4
JonahBurke <jonahburke777@gmail.com>
2020-11-06 12:01:47 -0600

    Updated ListAccessor with new functions, put my changes to ItemsController back in, deleted ItemEngine because it's bad and not getting used.

--- GroceryListService/Accessors/listAccessor.cs
+++ GroceryListService/Accessors/listAccessor.cs
@@ -10,68 +10,116 @@ namespace GroceryListService.Accessors
 
         public Boolean ListExists(string listName, User user)
         {
-            string query = "SELECT * FROM \"List\" as l JOIN \"User\" as u ON l.userId = u.userId WHERE l.\"name\" = @ListName AND u.\"email\" = @Email;";
-            using (SqlCommand cmd = new SqlCommand(query))
+            return SelectList(listName, user) != null;
+        }
+
+        public List SelectList(string listName, User user)
+        {
+            string query = "SELECT \"listId\" FROM \"List\" WHERE \"name\" = @ListName AND userId = @UserID;";
+            using SqlCommand cmd = new SqlCommand(query, GetConnection());
+            cmd.Parameters.Add("@ListName", System.Data.SqlDbType.NVarChar, 100);
+            cmd.Parameters.Add("@UserID", System.Data.SqlDbType.Int);
+
+            cmd.Parameters["@ListName"].Value = listName;
+            cmd.Parameters["@UserID"].Value = user.Id;
+            try
             {
-                cmd.Parameters.Add("@Email", System.Data.SqlDbType.NVarChar, 255);
-                cmd.Parameters.Add("@ListName", System.Data.SqlDbType.NVarChar, 100);
-
-                cmd.Parameters["@Email"].Value = user.Email;
-                cmd.Parameters["@ListName"].Value = listName;
-                cmd.Connection = GetConnection();
-                try
+                OpenConnection();
+                SqlDataReader reader = cmd.ExecuteReader();
+                List l = null;
+                while (reader.Read())
                 {
-                    base.OpenConnection();
-                    SqlDataReader reader = cmd.ExecuteReader();
-                    if (reader.HasRows)
+                    l = new List
                     {
-                        base.CloseConnection();
-                        return true;
-                    }
-                    else
-                    {
-                        base.CloseConnection();
-                        return false;
-                    }
+                        UserId = user.Id,
+                        Id = int.Parse(reader["listID"].ToString()),
+                        Name = listName
+                    };
+                }
+                reader.Close();
+                CloseConnection();
+                return l;
 
-                }
-                catch (SqlException)
-                {
-                    return false;
-                }
+            }
+            catch (SqlException)
+            {
+                return null;
             }
         }
-        public Boolean RemoveList(string listName, User user)
+
+        public void RemoveList(List list)
         {
-            string query = "DELETE FROM \"List\" as l JOIN \"User\" as u ON l.userId = u.userId WHERE l.\"name\" = @ListName AND u.\"email\" = @Email;";
-            using (SqlCommand cmd = new SqlCommand(query))
+            // Pretty sure we need to do this first
+            string itemDelete = "DELETE FROM \"Item\" WHERE listId = (SELECT \"listId\" FROM \"List\" WHERE \"name\" = @ListName AND userId = @UserID);";
+            string listDelete = "DELETE FROM \"List\" WHERE \"name\" = @ListName AND userId = @UserID;";
+            using SqlCommand cmd1 = new SqlCommand(itemDelete, GetConnection());
+            using SqlCommand cmd2 = new SqlCommand(listDelete, GetConnection());
+            cmd1.Parameters.Add("@ListName", System.Data.SqlDbType.NVarChar, 100);
+            cmd1.Parameters.Add("@UserID", System.Data.SqlDbType.Int);
+            cmd2.Parameters.Add("@ListName", System.Data.SqlDbType.NVarChar, 100);
+            cmd2.Parameters.Add("@UserID", System.Data.SqlDbType.Int);
+
+            cmd1.Parameters["@ListName"].Value = list.Name;
+            cmd1.Parameters["@UserID"].Value = list.UserId;
+            cmd2.Parameters["@ListName"].Value = list.Name;
+            cmd2.Parameters["@UserID"].Value = list.UserId;
+            try
             {
-                cmd.Parameters.Add("@Email", System.Data.SqlDbType.NVarChar, 255);
-                cmd.Parameters.Add("@ListName", System.Data.SqlDbType.NVarChar, 100);
+                OpenConnection();
+                SqlDataReader reader = cmd1.ExecuteReader();
+                reader = cmd2.ExecuteReader();
+                CloseConnection();
+                return;
+            }
+            catch (SqlException)
+            {
+                return;
+            }
+        }
 
-                cmd.Parameters["@Email"].Value = user.Email;
-                cmd.Parameters["@ListName"].Value = listName;
-                cmd.Connection = GetConnection();
-                try
-                {
-                    base.OpenConnection();
-                    SqlDataReader reader = cmd.ExecuteReader();
-                    if (reader.HasRows)
-                    {
-                        base.CloseConnection();
-                        return true;
-                    }
-                    else
-                    {
-                        base.CloseConnection();
-                        return false;
-                    }
+        public void UpdateList(List list)
+        {
+            string query = "UPDATE \"List\" SET \"name\" = @Name, userId = @UserID WHERE listId = @ListID;";
+            using SqlCommand cmd = new SqlCommand(query, GetConnection());
+            cmd.Parameters.Add("@ListName", System.Data.SqlDbType.NVarChar, 100);
+            cmd.Parameters.Add("@UserID", System.Data.SqlDbType.Int);
+            cmd.Parameters.Add("@ListID", System.Data.SqlDbType.Int);
 
-                }
-                catch (SqlException)
-                {
-                    return false;
-                }
+            cmd.Parameters["@Name"].Value = list.Name;
+            cmd.Parameters["@UserID"].Value = list.UserId;
+            cmd.Parameters["@ListID"].Value = list.Id;
+            try
+            {
+                OpenConnection();
+                SqlDataReader reader = cmd.ExecuteReader();
+                CloseConnection();
+                return;
+            }
+            catch (SqlException)
+            {
+                return;
+            }
+        }
+
+        public void InsertList(List list)
+        {
+            string query = "INSERT INTO \"List\"(\"name\", \"userId\") values (@ListName, @UserID);";
+            using SqlCommand cmd = new SqlCommand(query, GetConnection());
+            cmd.Parameters.Add("@ListName", System.Data.SqlDbType.NVarChar, 100);
+            cmd.Parameters.Add("@UserID", System.Data.SqlDbType.Int);
+
+            cmd.Parameters["@ListName"].Value = list.Name;
+            cmd.Parameters["@UserID"].Value = list.UserId;
+            try
+            {
+                OpenConnection();
+                SqlDataReader reader = cmd.ExecuteReader();
+                CloseConnection();
+                return;
+            }
+            catch (SqlException)
+            {
+                return;
             }
         }
     }
