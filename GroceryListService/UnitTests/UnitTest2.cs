a5f40e907a49b17971a52042ff79095eac833aa8
Jacob(Jiakai) <65819828+Jacob5286@users.noreply.github.com>
2020-11-18 22:29:49 -0600

    Create UnitTest2.cs

---
+++ GroceryListService/UnitTests/UnitTest2.cs
@@ -0,0 +1,46 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _GroceryListService.Data;
+using GroceryListService.Accessors;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GroceryListService.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ItemTest_getAllItem()
+        {
+            var controller = new ItemController(new MockItemRepo(), new NullLogger<ItemController>);
+            
+            var result = controller.getItem();
+            var goodResult = result as ObjectiveResult;
+            
+            Assert.AreEqual(300, goodResult.statuscode);
+        }
+
+        [TestMethod]
+        public void ListTest_getAllList()
+        {
+            var controller = new ListController(new MockListRepo(), new NullLogger<ListController>);
+            
+            var result = controller.getList(1);
+            var goodResult = result as ObjectiveResult;
+            
+            Assert.AreEqual(result, actual);
+        }
+        
+        /*[TestMethod]
+        public void home_page_test()
+        {
+            HomePage obj = new HomePage();
+            Assert.AreEqual(obj.Name, "GroceryListService Group-9");
+        }*/
+    }
+}
